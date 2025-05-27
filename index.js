228ad18a3fae7f8b01bbf9932e5fba0a35b57e08
Suchithra <125395288+SuchithraChandrasekaran@users.noreply.github.com>
2025-05-27 18:54:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const basicAuth = require('basic-auth');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => res.send('Hello, world, Dockerized Nodejs project (from EC2)'));
+
+app.get('/secret', (req, res) => {
+  const user = basicAuth(req);
+  if (!user || user.name !== process.env.USERNAME || user.pass !== process.env.PASSWORD) {
+    res.set('WWW-Authenticate', 'Basic realm="example"');
+    return res.status(401).send('Access denied');
+  }
+  res.send(process.env.SECRET_MESSAGE);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
